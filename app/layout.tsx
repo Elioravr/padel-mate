b37174f76d37ec33da357a784f07f010989e9c89
Elior Abramoviz <elior@fb.com>
2024-09-04 02:02:56 +0300

    Change app logo to jpg

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,17 +23,17 @@ export default function RootLayout({
           content='black-translucent'
         />
         <meta name='apple-mobile-web-app-title' content='PadelMate' />
-        <link rel='apple-touch-icon' sizes='57x57' href='app-logo.webp' />
-        <link rel='apple-touch-icon' sizes='114x114' href='app-logo.webp' />
-        <link rel='apple-touch-icon' sizes='72x72' href='app-logo.webp' />
-        <link rel='apple-touch-icon' sizes='144x144' href='app-logo.webp' />
-        <link rel='apple-touch-icon' sizes='60x60' href='app-logo.webp' />
-        <link rel='apple-touch-icon' sizes='120x120' href='app-logo.webp' />
-        <link rel='apple-touch-icon' sizes='76x76' href='app-logo.webp' />
-        <link rel='apple-touch-icon' sizes='152x152' href='app-logo.webp' />
+        <link rel='apple-touch-icon' sizes='57x57' href='app-logo-1.jpg' />
+        <link rel='apple-touch-icon' sizes='114x114' href='app-logo-1.jpg' />
+        <link rel='apple-touch-icon' sizes='72x72' href='app-logo-1.jpg' />
+        <link rel='apple-touch-icon' sizes='144x144' href='app-logo-1.jpg' />
+        <link rel='apple-touch-icon' sizes='60x60' href='app-logo-1.jpg' />
+        <link rel='apple-touch-icon' sizes='120x120' href='app-logo-1.jpg' />
+        <link rel='apple-touch-icon' sizes='76x76' href='app-logo-1.jpg' />
+        <link rel='apple-touch-icon' sizes='152x152' href='app-logo-1.jpg' />
 
         {/* For Android */}
-        <meta name='theme-color' content='#317EFB' />
+        <meta name='theme-color' content='#1d232a' />
         <link rel='manifest' href='/manifest.json' />
       </head>
       <body className={inter.className}>{children}</body>
