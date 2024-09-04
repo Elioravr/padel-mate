4bda6f91246b28f1cc9f71d775df5981dfeea1c3
Elior Abramoviz <elior@fb.com>
2024-09-04 15:29:46 +0300

    Explain exactly what is missing in /api/users POST

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -6,20 +6,43 @@ const db = new PrismaClient();
 export async function POST(req: Request) {
   try {
     // Parse the request body
-    const { id, first_name, last_name } = await req.json(); // Use req.json() to parse the body
+    const { id, first_name, last_name } = await req.json(); // Extract the fields you need from req.json()
+
+    // Make sure required fields exist
+    if (!id) {
+      return NextResponse.json(
+        { error: 'Missing required fields - id' },
+        { status: 400 }
+      );
+    }
+    if (!first_name) {
+      return NextResponse.json(
+        { error: 'Missing required fields - first_name' },
+        { status: 400 }
+      );
+    }
+    if (!last_name) {
+      return NextResponse.json(
+        { error: 'Missing required fields - last_name' },
+        { status: 400 }
+      );
+    }
 
     // Create a new player in the database
     const newPlayer = await db.player.create({
       data: {
+        id: id, // Clerk user ID
         firstName: first_name,
         lastName: last_name,
-        id: id,
         level: 0, // Default player level on sign-up
       },
     });
 
     return NextResponse.json({ player: newPlayer }, { status: 201 });
   } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
+    return NextResponse.json(
+      { error: `Failed to create player ${error}` },
+      { status: 500 }
+    );
   }
 }
