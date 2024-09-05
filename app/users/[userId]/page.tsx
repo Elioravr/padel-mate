4839ddc26e093ed201322e8a4929e1b8060987a1
Elior Abramoviz <elior@fb.com>
2024-09-06 02:53:35 +0300

    Add upcoming courts to the player page

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -21,6 +21,15 @@ const Page = async ({ params }: { params: { userId: string } }) => {
   );
   const courtesOwnedByPlayer: Court[] = await courtesOwnedByPlayerRes.json();
 
+  const upcomingCourtsRes = await fetch(
+    `${getBaseURL()}/api/courts/upcoming-courts?userId=${params.userId}`,
+    {
+      method: 'GET',
+      cache: 'no-store', // Disable caching
+    }
+  );
+  const upcomingCourts: Court[] = await upcomingCourtsRes.json();
+
   if (!response.ok) {
     return <div className='flex'>Player was not found</div>;
   }
@@ -29,6 +38,11 @@ const Page = async ({ params }: { params: { userId: string } }) => {
   return (
     <div className='flex flex-col p-2'>
       <PlayerItem player={player} />
+
+      <CourtsCarousel
+        title={`Upcoming Courts for ${player.firstName}`}
+        courts={upcomingCourts}
+      />
 
       <CourtsCarousel
         title={`Courts created by ${player.firstName}`}
