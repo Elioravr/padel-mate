d841b1e5da6d06a304428c617d881366c7673d14
Elior Abramoviz <elior@fb.com>
2024-09-06 04:10:12 +0300

    Fix timezone differences in Open Graph metadata

--- utils/util.ts
+++ utils/util.ts
@@ -1,33 +1,29 @@
 export function formatDate(dateAsString: Date): string {
   const date = new Date(dateAsString);
-  const daysOfWeek = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-  ];
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: 'long',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    timeZone: 'Asia/Jerusalem', // Specify the Israel time zone
+  };
 
-  const dayName = daysOfWeek[date.getDay()];
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
-  const year = date.getFullYear();
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-
-  return `${dayName}, ${day}/${month}/${year}, ${hours}:${minutes}`;
+  return new Intl.DateTimeFormat('en-IL', options).format(date);
 }
 
 export function formatShortDate(dateAsString: Date): string {
   const date = new Date(dateAsString);
 
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
-  const year = date.getFullYear();
+  const options: Intl.DateTimeFormatOptions = {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    timeZone: 'Asia/Jerusalem', // Specify the Israel time zone
+  };
 
-  return `${day}/${month}/${year}`;
+  return new Intl.DateTimeFormat('en-IL', options).format(date);
 }
 
 export function getBaseURL() {
