50766ca4dc7d14cf552f49c83842cf7f1792a50e
Elior Abramoviz <elior@fb.com>
2024-09-08 13:43:22 +0300

    Make the call to window in the SignInModal safe

--- components/SignInModal.tsx
+++ components/SignInModal.tsx
@@ -1,14 +1,23 @@
 'use client';
 
 import { SignIn } from '@clerk/nextjs';
+import { useEffect, useState } from 'react';
 
 const SignInModal = () => {
+  const [url, setUrl] = useState('');
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setUrl(window.location.href);
+    }
+  }, []);
+
   return (
     <div className='flex p-6 flex-col justify-center items-center fixed top-0 right-0 left-0 bottom-0 z-50 bg-[rgba(0,0,0,0.7)]'>
       <h2 className='font-bold text-lg mb-3'>
         You must sign in to use PadelMate
       </h2>
-      <SignIn routing='hash' forceRedirectUrl={window.location.href} />
+      <SignIn routing='hash' forceRedirectUrl={url} />
     </div>
   );
 };
