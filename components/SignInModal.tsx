a5282a91bed1a704d4ad475d99d52811c774e69a
Elior Abramoviz <elior@fb.com>
2024-09-08 17:22:00 +0300

    Fix redirect after signin

--- components/SignInModal.tsx
+++ components/SignInModal.tsx
@@ -1,23 +1,21 @@
 'use client';
 
-import { SignIn } from '@clerk/nextjs';
-import { useEffect, useState } from 'react';
+import { SignUp } from '@clerk/nextjs';
+import { getBaseURL } from '@utils/util';
+import { usePathname } from 'next/navigation';
 
 const SignInModal = () => {
-  const [url, setUrl] = useState('');
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setUrl(window.location.href);
-    }
-  }, []);
+  const path = usePathname();
 
   return (
     <div className='flex p-6 flex-col justify-center items-center fixed top-0 right-0 left-0 bottom-0 z-50 bg-[rgba(0,0,0,0.7)]'>
       <h2 className='font-bold text-lg mb-3'>
         You must sign in to use PadelMate
       </h2>
-      <SignIn routing='hash' forceRedirectUrl={url} />
+      <SignUp
+        routing='hash'
+        forceRedirectUrl={`${getBaseURL()}${path.slice(1)}`}
+      />
     </div>
   );
 };
